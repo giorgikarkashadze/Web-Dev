3151c9ef532dae35bbbe9aa2a92eec91ac6bd291
giorgikarkashadze <giorgi2004giorgiqarqashadze@gmail.com>
2023-07-01 19:22:49 +0400

    Finished website for desktop

--- beebp.io/js/main.js
+++ beebp.io/js/main.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
+
+    let header = document.querySelector('.header');
+    let headerHeight = header.clientHeight;
+
+window.onscroll = function(){
+    if(window.pageYOffset > headerHeight){
+        header.classList.add('fixed')
+    }else{
+        header.classList.remove('fixed')
+    }
+}
+
     $('.reviews__slider').slick({
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        fade: true,
+        speed:1000,
       });
   });
